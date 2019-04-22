848d3bb205fb89e13d23b25ab55e77d1b701cea1
Radya Al-Basha <47277078+RadyaAlbasha@users.noreply.github.com>
2019-04-22 18:20:58 +0200

    change
    test using web service

--- Day5/Sync&AsyncLab1/Sync&AsyncLab1/ViewController.m
+++ Day5/Sync&AsyncLab1/Sync&AsyncLab1/ViewController.m
@@ -34,7 +34,27 @@
 }
 
 - (IBAction)asyncAction:(UIButton *)sender {
-    NSURL * url = [NSURL URLWithString:@"https://maktoob.yahoo.com/?p=us"];
+    //NSURL * url = [NSURL URLWithString:@"https://maktoob.yahoo.com/?p=us"];
+    
+    //7atena permition fe info.plist 3shan el url (http) mesh (https)
+    /*
+     <key>NSAppTransportSecurity</key>
+     <dict>
+     <key>NSAllowsArbitraryLoads</key>
+     <true/>
+     <key>NSExceptionDomains</key>
+     <dict>
+     <key>example.com</key>
+     <dict>
+     <key>NSExceptionAllowsInsecureHTTPLoads</key>
+     <true/>
+     <key>NSIncludesSubdomains</key>
+     <true/>
+     </dict>
+     </dict>
+     </dict>
+     */
+    NSURL * url = [NSURL URLWithString:@"http://jets.iti.gov.eg/FriendsApp/services/user/register?name=yourName&phone=yourPhone"];
     NSURLRequest * request = [NSURLRequest requestWithURL:url];
     NSURLConnection * connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
 }
@@ -49,6 +69,7 @@
     //3shan el didReceiveData btetnada aktar men marra kol marra bygeb goz2 men el data
     [dataReceived appendData:data];
     
+    //de btgeb part men el data mesh kolha
    // NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
    // [_textView setText:str];
     
@@ -56,7 +77,11 @@
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection{
     // hena hkon mot2akeda eno 3ml load lel data kolha f a3redha
-    NSString *str = [[NSString alloc] initWithData:dataReceived encoding:NSUTF8StringEncoding];
+   // NSString *str = [[NSString alloc] initWithData:dataReceived encoding:NSUTF8StringEncoding];
+    
+    ///b3ml pars lel json w a2ra el data elly 3ayzaha
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:dataReceived options:NSJSONReadingAllowFragments error:nil];
+    NSString * str = [dict objectForKey:@"status"];
     [_textView setText:str];
 }
 
