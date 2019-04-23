a077ef8df6dd799627bb1f285cadcd7cb5be54b2
Radya Al-Basha <47277078+RadyaAlbasha@users.noreply.github.com>
2019-04-23 12:09:13 +0200

    add Annotation

---
+++ Day6/MapLab1/MapLab1/MADAnotation.m
@@ -0,0 +1,13 @@
+//
+//  MADAnotation.m
+//  MapLab1
+//
+//  Created by JETS Mobile Lab - 2 on 4/23/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import "MADAnotation.h"
+
+@implementation MADAnotation
+
+@end
--- Day6/MapLab1/MapLab1/ViewController.m
+++ Day6/MapLab1/MapLab1/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
-
+#import "MADAnotation.h"
 @interface ViewController ()
+- (IBAction)click:(id)sender;
 
 @end
 
@@ -18,7 +19,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    [_mapView setZoomEnabled:NO];
+   // [_mapView setZoomEnabled:NO];
     //[_mapView setDelegate:self];
     _mapView.delegate=self;
 }
@@ -29,5 +30,21 @@
 
 - (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated{
      printf("regionDidChangeAnimated\n");
+}
+
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view{
+    printf("didSelectAnnotationView\n");
+    
+    printf("%s\n",[view.annotation.title UTF8String]);
+}
+- (IBAction)click:(id)sender {
+    CGPoint touchPoint = [sender locationInView:_mapView];
+    //struct mesh class 3shan keda ma 7tetsh *
+    CLLocationCoordinate2D touchCoordinate = [_mapView convertPoint:touchPoint toCoordinateFromView:_mapView];
+    MADAnotation * myAnnotation = [MADAnotation new];
+    myAnnotation.coordinate = touchCoordinate;
+    myAnnotation.title = @"MAD";
+    myAnnotation.subtitle = @"JETS";
+    [_mapView addAnnotation:myAnnotation];
 }
 @end
