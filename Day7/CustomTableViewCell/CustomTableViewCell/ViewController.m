3804a72569d8a395a0b32c942014691b8ee4a4dc
Radya Al-Basha <47277078+RadyaAlbasha@users.noreply.github.com>
2019-04-24 10:28:40 +0200

    add
    view tabel

--- Day7/CustomTableViewCell/CustomTableViewCell/ViewController.m
+++ Day7/CustomTableViewCell/CustomTableViewCell/ViewController.m
@@ -12,12 +12,28 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+     NSArray *fristLabelArray;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+     fristLabelArray = @[@"Label one", @"Label two" , @"Label three"];
+    _myTabelView.delegate=self;
+    _myTabelView.dataSource=self;
 }
-
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return [fristLabelArray count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.textLabel.text = [fristLabelArray objectAtIndex:indexPath.row];
+    return cell;
+}
 
 @end
