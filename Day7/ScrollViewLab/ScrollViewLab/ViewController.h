7050d4b20a17b53a491e689c66eca05d96a44bec
Radya Al-Basha <47277078+RadyaAlbasha@users.noreply.github.com>
2019-04-24 11:36:51 +0200

    ScrollView

---
+++ Day7/ScrollViewLab/ScrollViewLab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewLab
+//
+//  Created by JETS Mobile Lab - 2 on 4/24/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Day7/ScrollViewLab/ScrollViewLab/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  ScrollViewLab
+//
+//  Created by JETS Mobile Lab - 2 on 4/24/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *myScrollView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [_myScrollView setScrollEnabled:YES];
+    [_myScrollView setContentSize:CGSizeMake(500,1000 )];
+}
+
+
+@end
